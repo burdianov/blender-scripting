4e18444a4cb8fbd08a1cdf04978272a7d4d4031a
Lilian Burdianov <lilian.burdianov@gmail.com>
2023-12-03 23:32:50 +0400

    v02

--- v02.py
+++ v02.py
@@ -1,5 +1,19 @@
+import math
+
 import bpy
 
 bpy.ops.mesh.primitive_cube_add(
     size=2, enter_editmode=False, align="WORLD", location=(0, 0, 0), scale=(1, 1, 1)
 )
+
+cube = bpy.context.active_object
+
+start_frame = 1
+cube.keyframe_insert("rotation_euler", frame=start_frame)
+
+degrees = 360
+radians = math.radians(degrees)
+cube.rotation_euler.z = radians
+
+end_frame = 180
+cube.keyframe_insert("rotation_euler", frame=end_frame)
